ef605b2643a2aac2605ac522fa30619a7f6967ad
je-moore <jacksonmoore@tuta.io>
2019-12-15 13:53:58 +0100

    click on modal to close

--- src/components/NewsModal.js
+++ src/components/NewsModal.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useFeaturedArticle } from '../hooks'
 import { Dialog } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 function NewsModal({ articleId }) {
+  const history = useHistory()
+  const closeDialog = () => {
+    history.push('/')
+  }
   const { loading, error, data } = useFeaturedArticle(articleId)
 
   return (
-    <Dialog fullScreen open={true}>
+    <Dialog fullScreen open={true} onClick={closeDialog}>
       {loading && <pre>loading</pre>}
       {error && <pre>error</pre>}
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
