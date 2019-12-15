cc8739e0bced10905ec076b305e3b0e50bc70348
je-moore <jacksonmoore@tuta.io>
2019-12-15 12:25:33 +0100

    implement material-ui Card components

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,21 +1,51 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
 const DEFAULT_IMAGE =
   'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg'
 
 function NewsCard({ article }) {
+  const classes = makeStyles({
+    card: {
+      margin: '20px',
+    },
+    title: {
+      fontSize: 14,
+    },
+    description: {
+      fontSize: 12,
+    },
+    cardImg: {
+      width: '100%',
+      boxSizing: 'border-box',
+    },
+  })()
+
   return (
-    <div>
-      <img
-        src={
-          article.imageUrl
-            ? `https://fashionunited.com${article.imageUrl}`
-            : DEFAULT_IMAGE
-        }
-        alt=""
-      />
-      <h5>{article.title}</h5>
-    </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <img
+          className={classes.cardImg}
+          src={
+            article.imageUrl
+              ? `https://fashionunited.com${article.imageUrl}`
+              : DEFAULT_IMAGE
+          }
+          alt=""
+        />
+        <Typography className={classes.title} color="textPrimary" gutterBottom>
+          {article.title}
+        </Typography>
+        <Typography
+          className={classes.description}
+          color="textSecondary"
+          gutterBottom
+        >
+          {article.description}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import NewsCard from './NewsCard'
+import { makeStyles } from '@material-ui/core/styles'
 
 function NewsFeed({ data }) {
+  const classes = makeStyles({
+    cardContainer: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
+    },
+  })()
+
   return (
-    <div>
+    <div className={classes.cardContainer}>
       {data.fashionunitedNlNewsArticles.map(article => (
         <NewsCard article={article} key={article.id} />
       ))}
