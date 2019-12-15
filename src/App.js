ba2a86dc78d1650e0040f10c5e08421f270447ff
je-moore <jacksonmoore@tuta.io>
2019-12-15 11:44:19 +0100

    set up Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import { useParams } from 'react-router-dom'
 
-function App() {
+const NEWS_FEED = gql`
+  query NewsArticles($offset: Int, $limit: Int, $keywords: [String]) {
+    fashionunitedNlNewsArticles(
+      keywords: $keywords
+      limit: $limit
+      offset: $offset
+    ) {
+      title
+      url
+      imageUrl
+      description
+      id
+    }
+  }
+`
+
+const App = () => {
+  const { loading, error, data, fetchMore } = useQuery(NEWS_FEED, {
+    partialRefetch: true,
+    returnPartialData: true,
+  })
+
   return (
     <div className="App">
       <header>Fashion News</header>
+      <main>
+        {loading && <pre>loading</pre>}
+        {error && <pre>error</pre>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      </main>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+  uri: 'https://fashionunited.info/graphql/',
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
